dacb8ae9bd80a47eec418f3796bcdda715e1cfdc
ldgeao99 <ldgeao99@gmail.com>
2019-02-22 15:59:13 +0900

    앱 초기화, 로그인, 로그아웃 기능까지 구현됨

---
+++ mysite/kilogram/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+
+class IndexView(TemplateView): # 제네릭의 TemplateView는 아무기능 없이 템플릿만 표시해 주는 뷰에서 사용
+    template_name = 'kilogram/index.html'
