41ba5f482e8fffb05db857a16e02013523cc8e59
ldgeao99 <ldgeao99@gmail.com>
2019-02-26 22:09:00 +0900

    from .forms import CreateUserForm, UploadForm from django.contrib.auth.decorators import login_required 을 추가해 주었고, upload메소드를 생성하였음.

--- mysite/kilogram/views.py
+++ mysite/kilogram/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm # 장고의 기본적인 회원가입 폼. id, password만 확인한다는 한계점.
 from django.core.urlresolvers import reverse_lazy
-from .forms import CreateUserForm
+from .forms import CreateUserForm, UploadForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def upload(request):
+    if request.method == "POST":
+        form = UploadForm(request.POST, request.FILES) # 대용량인 이미지를 처리해야 하므로 두 매개변수를 넘겨줘야함.
+        if form.is_valid():
+            photo = form.save(commit=False) # photo객체를 가져오긴 하나 DB에 아직 저장하진 않음
+            photo.owner = request.user      # request.user는 로그인한 사용자
+            form.save()
+            return redirect('kilogram:index')
+
+
+    form = UploadForm()
+    return render(request, 'kilogram/upload.html', {'form' : form})
 
 class IndexView(TemplateView):     # 제네릭의 TemplateView는 아무기능 없이 템플릿만 표시해 주는 뷰에서 사용
     template_name = 'kilogram/index.html'
