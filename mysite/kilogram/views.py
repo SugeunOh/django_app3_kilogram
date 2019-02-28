1df249b19149f0233cb24c2b68abb2c30484946e
ldgeao99 <ldgeao99@gmail.com>
2019-03-01 02:27:54 +0900

    DetailView, View, UserForm, ProfileForm 모듈 import ProfileUpdateView클래스 추가

--- mysite/kilogram/views.py
+++ mysite/kilogram/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm # 장고의 기본적인 회원가입 폼. id, password만 확인한다는 한계점.
@@ -8,6 +8,8 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic.list import ListView
 from django.contrib.auth.models import User
 from django.views.generic.detail import DetailView
+from django.views import View
+from .forms import UserForm, ProfileForm
 # Create your views here.
 
 @login_required   # 메소드에만 적용가능
@@ -53,3 +55,47 @@ class ProfileView(DetailView):
     context_object_name = 'profile_user' # model로 지정해준 User모델에 대한 객체와 로그인한 사용자랑 명칭이 겹쳐버리기 때문에 이를 지정해줌.
     model = User
     template_name = 'kilogram/profile.html'
+
+
+class ProfileUpdateView(View): # 간단한 View클래스를 상속 받았으므로 get함수와 post함수를 각각 만들어줘야한다.
+    # 프로필 편집에서 보여주기위한 get 메소드
+    def get(self, request):
+        user = get_object_or_404(User, pk=request.user.pk)  # 로그인중인 사용자 객체를 얻어옴
+        user_form = UserForm(initial={
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        })
+
+        if hasattr(user, 'profile'):  # user가 profile을 가지고 있으면 True, 없으면 False (회원가입을 한다고 profile을 가지고 있진 않으므로)
+            profile = user.profile
+            profile_form = ProfileForm(initial={
+                'nickname': profile.nickname,
+                'profile_photo': profile.profile_photo,
+            })
+        else:
+            profile_form = ProfileForm()
+
+        return render(request, 'kilogram/profile_update.html', {"user_form": user_form, "profile_form": profile_form})
+
+    # 프로필 편집에서 실제 수정(저장) 버튼을 눌렀을 때 넘겨받은 데이터를 저장하는 post 메소드
+    def post(self, request):
+        u = User.objects.get(id=request.user.pk)        # 로그인중인 사용자 객체를 얻어옴
+        user_form = UserForm(request.POST, instance=u)  # 기존의 것의 업데이트하는 것 이므로 기존의 인스턴스를 넘겨줘야한다. 기존의 것을 가져와 수정하는 것
+
+        # User 폼
+        if user_form.is_valid():
+            user_form.save()
+
+        if hasattr(u, 'profile'):
+            profile = u.profile
+            profile_form = ProfileForm(request.POST, request.FILES, instance=profile) # 기존의 것 가져와 수정하는 것
+        else:
+            profile_form = ProfileForm(request.POST, request.FILES) # 새로 만드는 것
+
+        # Profile 폼
+        if profile_form.is_valid():
+            profile = profile_form.save(commit=False) # 기존의 것을 가져와 수정하는 경우가 아닌 새로 만든 경우 user를 지정해줘야 하므로
+            profile.user = u
+            profile.save()
+
+        return redirect('kilogram:profile', pk=request.user.pk) # 수정된 화면 보여주기
